4e6ab9142a743f8125ac6c8f46ff6e5d77913a52
Brian Vetter <41239521+bvett@users.noreply.github.com>
2026-08-11 16:10:17 -0400

    create Viewer base class

--- src/value_stream/utils/result_viewer.py
+++ src/value_stream/utils/result_viewer.py
@@ -1,77 +1,23 @@
-from typing import Any, Optional
-from enum import Enum
+from typing import Optional
 import colorsys
-import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib import ticker
 import numpy as np
-from pandas import json_normalize
 from tqdm import tqdm
 from ..event_status import EventStatus
 from ..simulation_result import SimulationResult
 from ..task import TaskType
 from ..task_event import TaskEvent
-from ..workflow_state_name import WorkflowStateName
+
+from .viewer import Viewer
 
 
-class ResultViewer:
+class ResultViewer(Viewer):
     """Handles rendering of simulation results"""
 
     def __init__(self, results: list[SimulationResult], pbar: Optional[tqdm] = None, colormap='plasma'):
-
-        self.colormap = matplotlib.colormaps[colormap]
-        results_dict: list[dict[str, Any]] = []
-
-        colors = iter(self.colormap(
-            np.linspace(0.1, 0.9, len(WorkflowStateName))))
-
-        self.statecolor_map = {
-            WorkflowStateName.PENDING: next(colors),
-            WorkflowStateName.DEVELOPMENT: next(colors),
-            WorkflowStateName.DEV_COMPLETE: next(colors),
-            WorkflowStateName.QA_TESTING: next(colors),
-            WorkflowStateName.QA_COMPLETE: next(colors),
-            WorkflowStateName.DEPLOYMENT: next(colors)
-        }
-
-        self.label_map = {
-            WorkflowStateName.PENDING: 'waiting for dev',
-            WorkflowStateName.DEVELOPMENT: 'development',
-            WorkflowStateName.DEV_COMPLETE: 'waiting for qa',
-            WorkflowStateName.QA_TESTING: 'qa',
-            WorkflowStateName.QA_COMPLETE: 'waiting for delivery',
-            WorkflowStateName.DEPLOYMENT: 'delivery'
-        }
-
-        self.edgecolor_map = {
-            EventStatus.SUCCESS: 'none',
-            EventStatus.FAILURE: 'red'
-        }
-
-        for r in results:
-            results_dict.append(_to_dict(r, ['toolchain_pool']))
-
-            if pbar:
-                pbar.update()
-
-        self.df = json_normalize(results_dict, record_path=['events'],
-                                 meta=[['model', 'deployment_cadence'],
-                                       ['model', 'team_size'],
-                                       ['task', 'task_name'],
-                                       ['task', 'loss'],
-                                       ['task', 'delivered_value'],
-                                       ['task', 'task_type']],
-                                 errors='ignore')
-
-        self.df.set_index(['model.deployment_cadence',
-                           'model.team_size', 'task.task_name'], inplace=True)
-
-        self.df.sort_index(inplace=True)
-
-        self.df["event_duration"] = self.df.groupby(level=2)['time'].diff()
-        self.df["cumulative_time"] = self.df.groupby(
-            level=2)['event_duration'].cumsum()
+        super().__init__(results=results, pbar=pbar, colormap=colormap)
 
         self.df_workflow_stages = \
             self.df.loc[(self.df['event_type'] == TaskEvent.EventType.END) &
@@ -307,22 +253,3 @@ class ResultViewer:
         color_hls = colorsys.rgb_to_hls(color[0], color[1], color[2])
 
         return 'white' if color_hls[1] < 0.5 else 'black'
-
-
-def _to_dict(obj: Any, exclusions: list[str] = []):
-
-    if isinstance(obj, list):
-        return [_to_dict(o) for o in obj]
-
-    if isinstance(obj, Enum):
-        return str(obj)
-
-    if hasattr(obj, '__dict__'):
-        result: dict[str, Any] = {}
-
-        for _, (k, v) in enumerate(obj.__dict__.items()):
-            if k not in exclusions:
-                result[k] = _to_dict(v, exclusions)
-
-        return result
-    return obj
---
+++ src/value_stream/utils/viewer.py
@@ -0,0 +1,88 @@
+from enum import Enum
+import matplotlib
+import numpy as np
+
+from pandas import json_normalize
+from tqdm import tqdm
+from typing import Any, Optional
+
+from ..event_status import EventStatus
+from ..simulation_result import SimulationResult
+from ..workflow_state_name import WorkflowStateName
+
+
+class Viewer:
+
+    def __init__(self, results: list[SimulationResult], pbar: Optional[tqdm] = None, colormap='plasma'):
+
+        self.colormap = matplotlib.colormaps[colormap]
+        results_dict: list[dict[str, Any]] = []
+
+        colors = iter(self.colormap(
+            np.linspace(0.1, 0.9, len(WorkflowStateName))))
+
+        self.statecolor_map = {
+            WorkflowStateName.PENDING: next(colors),
+            WorkflowStateName.DEVELOPMENT: next(colors),
+            WorkflowStateName.DEV_COMPLETE: next(colors),
+            WorkflowStateName.QA_TESTING: next(colors),
+            WorkflowStateName.QA_COMPLETE: next(colors),
+            WorkflowStateName.DEPLOYMENT: next(colors)
+        }
+
+        self.label_map = {
+            WorkflowStateName.PENDING: 'waiting for dev',
+            WorkflowStateName.DEVELOPMENT: 'development',
+            WorkflowStateName.DEV_COMPLETE: 'waiting for qa',
+            WorkflowStateName.QA_TESTING: 'qa',
+            WorkflowStateName.QA_COMPLETE: 'waiting for delivery',
+            WorkflowStateName.DEPLOYMENT: 'delivery'
+        }
+
+        self.edgecolor_map = {
+            EventStatus.SUCCESS: 'none',
+            EventStatus.FAILURE: 'red'
+        }
+
+        for r in results:
+            results_dict.append(_to_dict(r, ['toolchain_pool']))
+
+            if pbar:
+                pbar.update()
+
+        self.df = json_normalize(results_dict, record_path=['events'],
+                                 meta=[['model', 'deployment_cadence'],
+                                       ['model', 'team_size'],
+                                       ['task', 'task_name'],
+                                       ['task', 'loss'],
+                                       ['task', 'delivered_value'],
+                                       ['task', 'task_type']],
+                                 errors='ignore')
+
+        self.df.set_index(['model.deployment_cadence',
+                           'model.team_size', 'task.task_name'], inplace=True)
+
+        self.df.sort_index(inplace=True)
+
+        self.df["event_duration"] = self.df.groupby(level=2)['time'].diff()
+        self.df["cumulative_time"] = self.df.groupby(
+            level=2)['event_duration'].cumsum()
+
+
+def _to_dict(obj: Any, exclusions: list[str] = []):
+
+    if isinstance(obj, list):
+        return [_to_dict(o) for o in obj]
+
+    if isinstance(obj, Enum):
+        return str(obj)
+
+    if hasattr(obj, '__dict__'):
+        result: dict[str, Any] = {}
+
+        for _, (k, v) in enumerate(obj.__dict__.items()):
+            if k not in exclusions:
+                result[k] = _to_dict(v, exclusions)
+
+        return result
+    return obj
