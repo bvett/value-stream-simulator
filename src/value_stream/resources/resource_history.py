69d7ac21eaa9faf56699c1ca3496a31aea53961c
Brian Vetter <41239521+bvett@users.noreply.github.com>
2026-08-28 17:26:57 -0400

    move ResourceHistory into own module

---
+++ src/value_stream/resources/resource_history.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from value_stream.core import WorkflowStateName
+
+
+class ResourceHistory:
+
+    def __init__(self,
+                 time: float,
+                 state: WorkflowStateName,
+                 allocated: int = 0,
+                 active: int = 0,
+                 success_t: Optional[float] = None,
+                 failure_t: Optional[float] = None,
+                 interruption_t: Optional[float] = None,
+                 waiting_t: Optional[float] = None,
+                 idle_t: Optional[float] = None):
+
+        self.time = time
+        self.state = state
+        self.allocated = allocated
+        self.active = active
+        self.success_t = success_t
+        self.failure_t = failure_t
+        self.interruption_t = interruption_t
+        self.waiting_t = waiting_t
+        self.idle_t = idle_t
--- src/value_stream/resources/resource_tracker.py
+++ src/value_stream/resources/resource_tracker.py
@@ -1,35 +1,12 @@
 import uuid
-
-from simpy import Environment
 from typing import Optional, Self
 
-from value_stream.core import EventStatus, WorkflowStateName
+from simpy import Environment
 
+from value_stream.core import EventStatus
+
+from .resource_history import ResourceHistory
 from .trackable import Trackable
-
-
-class ResourceHistory:
-
-    def __init__(self,
-                 time: float,
-                 state: WorkflowStateName,
-                 allocated: int = 0,
-                 active: int = 0,
-                 success_t: Optional[float] = None,
-                 failure_t: Optional[float] = None,
-                 interruption_t: Optional[float] = None,
-                 waiting_t: Optional[float] = None,
-                 idle_t: Optional[float] = None):
-
-        self.time = time
-        self.state = state
-        self.allocated = allocated
-        self.active = active
-        self.success_t = success_t
-        self.failure_t = failure_t
-        self.interruption_t = interruption_t
-        self.waiting_t = waiting_t
-        self.idle_t = idle_t
 
 
 class Tracker:
