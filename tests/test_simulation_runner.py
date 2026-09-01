589846e19a49e2c72f46d0becf7a327af2526d30
Brian Vetter <41239521+bvett@users.noreply.github.com>
2026-09-01 19:36:06 -0400

    mocking out Simulation in SimulationRunner test

--- tests/test_simulation_runner.py
+++ tests/test_simulation_runner.py
@@ -1,24 +1,25 @@
 import unittest
+from unittest.mock import patch
 from tqdm import tqdm
 from value_stream.client import SimulationRunner
-from value_stream.core import EventStatus, WorkflowStateName
 from value_stream.resources import QATester, Toolchain, DeveloperFactory
 from value_stream.simulation import ModelFactory
-from value_stream.task import TaskType, TaskEvent, TaskFactory
+from value_stream.task import TaskFactory
 
 # pylint:disable=missing-class-docstring,missing-function-docstring
 
 
 class TestSimulationRunner(unittest.TestCase):
 
-    def test_all(self):
+    @patch('value_stream.simulation.Simulation.execute')
+    def test_all(self, mock_simulation_execute):
 
         NUM_TASKS = 10
         NUM_DEVELOPERS = 2
         MAX_CADENCE = 5
         SUPPORT_INTERVAL = 4
 
-        simulation = SimulationRunner()
+        simulation_runner = SimulationRunner()
 
         teams = [DeveloperFactory().create(
             count=NUM_DEVELOPERS, efficiency=1.0)]
@@ -39,30 +40,9 @@ class TestSimulationRunner(unittest.TestCase):
                             story_points=1.0).create(count=NUM_TASKS)
 
         with tqdm(total=len(models)) as pbar:
-            simulation_results = simulation.execute(
+            _ = simulation_runner.execute(
                 tasks=tasks,
                 models=models,
                 pbar=pbar)
 
-        # one result for every combination of task and cadence
-        expected_dev_tasks = NUM_TASKS * (MAX_CADENCE + 1)
-
-        # hard-coding this to an arbitrary value for now to guard against regression
-        # will evolve to something more calculated when this tests individual simulations instead of the runner
-        expected_support_tasks = 8
-
-        num_dev_tasks = 0
-        num_support_tasks = 0
-
-        # The problem here is that we don't have access to the TaskType anymore.....
-        for r in simulation_results:
-            for e in r.metadata.event_metadata:
-                if (e.event == WorkflowStateName.DEVELOPMENT) and (e.status == EventStatus.SUCCESS) and (e.event_type == TaskEvent.EventType.END) and (e.task_type == TaskType.DEVELOPMENT):
-                    num_dev_tasks += 1
-                elif (e.event == WorkflowStateName.DEVELOPMENT) and (e.status == EventStatus.SUCCESS) and (e.event_type == TaskEvent.EventType.END) and (e.task_type == TaskType.SUPPORT):
-                    num_support_tasks += 1
-
-        self.assertEqual(num_dev_tasks, expected_dev_tasks)
-
-        # TODO: address flaky assert
-        # self.assertEqual(num_support_tasks, expected_support_tasks)
+        self.assertEqual(mock_simulation_execute.call_count, len(models))
