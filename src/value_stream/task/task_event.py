c6569c1360b2a540f882751a31219f936f91dc9b
Brian Vetter <41239521+bvett@users.noreply.github.com>
2026-09-04 17:05:35 -0400

    add resource_id to TaskEvent

--- src/value_stream/task/task_event.py
+++ src/value_stream/task/task_event.py
@@ -1,5 +1,6 @@
 from enum import StrEnum
 from typing import Optional
+from uuid import UUID
 
 from value_stream.core import EventStatus, TaskType, WorkflowStateName
 
@@ -13,7 +14,7 @@ class TaskEvent:
         END = 'end'
         TERMINAL = 'terminal'
 
-    def __init__(self, event: WorkflowStateName, event_type: EventType, time: float, status: EventStatus, task_type: TaskType, is_rework: bool, duration: Optional[float] = None, loss: float = 0):
+    def __init__(self, event: WorkflowStateName, event_type: EventType, time: float, status: EventStatus, task_type: TaskType, is_rework: bool, duration: Optional[float] = None, loss: float = 0, resource_id: Optional[UUID] = None):
         self.event: WorkflowStateName = event
         self.event_type: TaskEvent.EventType = event_type
         self.time: float = time
@@ -22,15 +23,16 @@ class TaskEvent:
         self.task_type: TaskType = task_type
         self.is_rework: bool = is_rework
         self.duration: Optional[float] = duration
+        self.resource_id: Optional[UUID] = resource_id
 
     @classmethod
-    def start(cls, event: WorkflowStateName, time: float, task_type: TaskType, is_rework: bool, status: EventStatus = EventStatus.SUCCESS) -> "TaskEvent":
-        return TaskEvent(event=event, time=time, status=status, event_type=TaskEvent.EventType.START, task_type=task_type, is_rework=is_rework)
+    def start(cls, event: WorkflowStateName, time: float, task_type: TaskType, is_rework: bool, status: EventStatus = EventStatus.SUCCESS, resource_id: Optional[UUID] = None) -> "TaskEvent":
+        return TaskEvent(event=event, time=time, status=status, event_type=TaskEvent.EventType.START, task_type=task_type, is_rework=is_rework, resource_id=resource_id)
 
     @classmethod
-    def end(cls, event: WorkflowStateName, time: float, task_type: TaskType, is_rework: bool, duration: float, status: EventStatus = EventStatus.SUCCESS, loss: float = 0) -> "TaskEvent":
-        return TaskEvent(event=event, time=time, status=status, event_type=TaskEvent.EventType.END, loss=loss, task_type=task_type, is_rework=is_rework, duration=duration)
+    def end(cls, event: WorkflowStateName, time: float, task_type: TaskType, is_rework: bool, duration: float, status: EventStatus = EventStatus.SUCCESS, loss: float = 0, resource_id: Optional[UUID] = None) -> "TaskEvent":
+        return TaskEvent(event=event, time=time, status=status, event_type=TaskEvent.EventType.END, loss=loss, task_type=task_type, is_rework=is_rework, duration=duration, resource_id=resource_id)
 
     @classmethod
-    def terminal(cls, event: WorkflowStateName, time: float, task_type: TaskType, is_rework: bool, status: EventStatus = EventStatus.SUCCESS) -> "TaskEvent":
-        return TaskEvent(event=event, time=time, status=status, event_type=TaskEvent.EventType.TERMINAL, task_type=task_type, is_rework=is_rework)
+    def terminal(cls, event: WorkflowStateName, time: float, task_type: TaskType, is_rework: bool, status: EventStatus = EventStatus.SUCCESS, resource_id: Optional[UUID] = None) -> "TaskEvent":
+        return TaskEvent(event=event, time=time, status=status, event_type=TaskEvent.EventType.TERMINAL, task_type=task_type, is_rework=is_rework, resource_id=resource_id)
