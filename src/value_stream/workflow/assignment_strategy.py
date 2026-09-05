17614558faf1b60303894e4c670788683ce48762
Brian Vetter <41239521+bvett@users.noreply.github.com>
2026-09-04 21:04:05 -0400

    introducing additional AssignmentStrategy enums

--- src/value_stream/workflow/assignment_strategy.py
+++ src/value_stream/workflow/assignment_strategy.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class AssignmentStrategy(Enum):
-    """Specifies how support work is assigned to a set of developers"""
+    """Specifies how a task is assigned to a resource"""
     RANDOM = 1
     CYCLIC = 2  # round-robin for a more even distribution of workload
+    OWNER = 3
+    NEXT_AVAILABLE = 4
