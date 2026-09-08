9972433e802aa1fa18f6deea4bcf613c9eb000d3
Brian Vetter <41239521+bvett@users.noreply.github.com>
2026-09-08 16:46:37 -0400

    split off DefaultSimulationPolicy into own package

--- src/value_stream/simulation/__init__.py
+++ src/value_stream/simulation/__init__.py
@@ -1,8 +1,9 @@
+from .default_simulation_policy import DefaultSimulationPolicy
 from .model import Model
 from .model_factory import ModelFactory
 from .simulation import Simulation
 from .simulation_metadata import SimulationMetadata
-from .simulation_policy import SimulationPolicy, DefaultSimulationPolicy
+from .simulation_policy import SimulationPolicy
 from .simulation_result import SimulationResult, SummaryResult
 
 __all__ = ["DefaultSimulationPolicy",
--- src/value_stream/simulation/simulation_policy.py
+++ src/value_stream/simulation/simulation_policy.py
@@ -1,5 +1,5 @@
 from value_stream.resources import ResourcePolicy
-from value_stream.task import Task, TaskType
+from value_stream.task import Task
 from value_stream.workflow import AssignmentStrategy, WorkflowPolicy
 
 
@@ -11,54 +11,3 @@ class SimulationPolicy(ResourcePolicy, WorkflowPolicy):
 
     def support_assignment_strategy(self, tasks: list[Task]) -> AssignmentStrategy:
         raise NotImplementedError
-
-
-class DefaultSimulationPolicy(SimulationPolicy):
-    """encapsulates control logic for a simulation"""
-
-    def task_priority(self, tasks_1: list[Task], tasks_2: list[Task]):
-        """Compares two Task lists and returns an indicator of which has higher priority
-
-        Returns:
-            int: -1 if tasks_1 is of higher priority, 
-            0 if tasks_1 and tasks_2 are of equal priority, and 
-            1 if tasks_2 is of higher priority
-        """
-
-        # handle use-cases when one or both lists are empty
-        if (not tasks_1) and (not tasks_2):
-            return 0
-
-        if not tasks_1:
-            return 1
-
-        if not tasks_2:
-            return -1
-
-        # assume homogeneous collections regarding TaskType
-        # 1) support is a higher priortiy than development
-        # 2) support (in tasks_1) is a higher priority than support (in tasks_2)
-        # 3) development is the same priority as development
-
-        tt_1 = tasks_1[0].task_type
-        tt_2 = tasks_2[0].task_type
-
-        if tt_1 == TaskType.SUPPORT:
-            return -1
-
-        if tt_2 == TaskType.SUPPORT:
-            return 1
-
-        return 0
-
-    def support_assignment_strategy(self, tasks: list[Task]) -> AssignmentStrategy:
-        if tasks:
-            task = tasks[0]
-
-            if (task.task_type == TaskType.DEVELOPMENT) and (task.is_rework is True):
-                return AssignmentStrategy.OWNER
-
-            if task.task_type == TaskType.SUPPORT:
-                return AssignmentStrategy.RANDOM
-
-        return AssignmentStrategy.NEXT_AVAILABLE
