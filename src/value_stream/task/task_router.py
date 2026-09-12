fd1bc89fc51688f1fa4df7fd742a9881cdf86735
Brian Vetter <41239521+bvett@users.noreply.github.com>
2026-09-12 10:36:50 -0400

    simplify TaskRouter

--- src/value_stream/task/task_router.py
+++ src/value_stream/task/task_router.py
@@ -1,4 +1,4 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 from typing import Optional
 from simpy import Store
@@ -9,19 +9,14 @@ from .task import Task
 from .task_type import TaskType
 
 
-class TaskRouter:
-    def __init__(self, route_1: 'Store | TaskRouter', route_2: Optional['Store | TaskRouter']):
-        self._route_1 = route_1
-        self._route_2 = route_2
+class TaskRouter(ABC):
 
     @abstractmethod
     def route(self, task: Task, status: Optional[EventStatus] = None) -> StorePut:
         pass
 
     @classmethod
-    def _send_to(cls, task: Task, route: Optional['Store | TaskRouter'], status: Optional[EventStatus] = None) -> StorePut:
-        if route is None:
-            raise RuntimeError("route is None")
+    def _send_to(cls, task: Task, route: 'Store | TaskRouter', status: Optional[EventStatus] = None) -> StorePut:
 
         if isinstance(route, Store):
             return route.put(task)
@@ -29,36 +24,36 @@ class TaskRouter:
         if isinstance(route, TaskRouter):
             return route.route(task=task, status=status)
 
-        raise RuntimeError("Unknown route type")
-
 
 class DefaultRouter(TaskRouter):
     def __init__(self, route: 'Store | TaskRouter'):
-        super().__init__(route_1=route, route_2=None)
+        self._route = route
 
     def route(self, task: Task, status: Optional[EventStatus] = None) -> StorePut:
-        return self._send_to(task, self._route_1, status)
+        return self._send_to(task, self._route, status)
 
 
 class StatusRouter(TaskRouter):
     def __init__(self, on_success: 'Store | TaskRouter', on_failure: 'Store | TaskRouter'):
-        super().__init__(route_1=on_success, route_2=on_failure)
+        self._on_success = on_success
+        self._on_failure = on_failure
 
     def route(self, task: Task, status: Optional[EventStatus] = EventStatus.FAILURE) -> StorePut:
 
         if status == EventStatus.SUCCESS:
-            return self._send_to(task, self._route_1, status)
+            return self._send_to(task, self._on_success, status)
 
-        return self._send_to(task, self._route_2, status)
+        return self._send_to(task, self._on_failure, status)
 
 
 class TypeRouter(TaskRouter):
     def __init__(self, on_support: 'Store | TaskRouter', on_development: 'Store | TaskRouter'):
-        super().__init__(route_1=on_support, route_2=on_development)
+        self._on_support = on_support
+        self._on_failure = on_development
 
     def route(self, task: Task, status: Optional[EventStatus] = None) -> StorePut:
 
         if task.task_type == TaskType.SUPPORT:
-            return self._send_to(task, self._route_1, status)
+            return self._send_to(task, self._on_support, status)
 
-        return self._send_to(task, self._route_2, status)
+        return self._send_to(task, self._on_failure, status)
